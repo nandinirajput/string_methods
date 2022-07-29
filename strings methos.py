313dabc4dfe0a00612b6999188884a2e3c4926ff
RajputNandini <109655286+nandinirajput@users.noreply.github.com>
2022-07-29 13:27:01 +0530

    Add files via upload

---
+++ strings methos.py
@@ -0,0 +1,31 @@
+# a='python'
+# print(a.upper())
+
+# print('python'.upeer())
+# print('PYTHON'.lower())
+# print('PROGRAM'.casefold())
+# print('mobile'.isupper())
+# print('MOBILE'.isupper())
+# print('mobile'.islower())
+# print('mobile'.islower())
+# print('PytHon'.swapcase())
+# print('pytHOn coDe'.capitalize())
+# print('pyTHOn coDe'.title())
+# print('Real Me'.istitle())
+# print('Real me'.istitle())
+# print('python
+# program'.startswith('F))
+# print('python
+# program'.startswith('p))
+# print('python
+# program'.startswith('pyt))
+# print('python
+# program'.endswith('pyt))
+# print('python
+# program'.endswith('am))
+# print('python
+# program'.endswith('gram'))
+# print(len('python'))
+# print('python'.find('h'))
+# print('python'.find('h'))
+# print('python'.find(T))
